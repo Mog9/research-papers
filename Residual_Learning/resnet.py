5650cc5d698327d0ab01cf2088b1a3b0ca15cb28
mohit <mohit.gyanchandani09@gmail.com>
2025-12-22 20:42:08 +0530

    resnet block

--- Residual_Learning/resnet.py
+++ Residual_Learning/resnet.py
@@ -1,4 +1,50 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F 
+from base_block import BasicBlock
 
+class Resnet18(nn.Module):
+    def __init__(self, num_classes=10):
+        super().__init__()
+        self.in_channels = 16
+
+        #stem
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=3, bias=False)
+        self.bn1 = nn.BatchNorm2d(16)
+        self.relu = nn.ReLU(inplace=False)
+
+        #residual layer (2 blocks each, so 8 total)
+        self.layer1 = self._make_layer(16, 2, stride=1)
+        self.layer2 = self._make_layer(32, 2, stride=2)
+        self.layer3 = self._make_layer(64, 2, stride=2)
+        self.layer4 = self._make_layer(128, 2, stride=2)
+
+        #head
+        self.avgpool = nn.AdaptiveAvgPool2d((1,1))
+        self.fc = nn.Linear(128, num_classes)
+
+    def _make_layer(self, out_channels, blocks, stride):
+        layers = []
+
+        layers.append(BasicBlock(self.in_channels, out_channels, stride))
+        self.in_channels = out_channels
+
+        for _ in range(1, blocks):
+            layers.append(BasicBlock(self.in_channels, out_channels, stride=1))
+        return nn.Sequential(*layers)
+    
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.layer4(x)
+
+        x = self.avgpool(x)
+        x = torch.flatten(x, 1)
+        x = self.fc(x)
+
+        return x
